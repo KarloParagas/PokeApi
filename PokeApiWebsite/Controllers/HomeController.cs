ad5d87ae92b483f6cf53333a115166d8aac7c1a8
KarloParagas <karloparagas@gmail.com>
2020-04-29 08:43:30 -0700

    Created Pokedex page

--- PokeApiWebsite/Controllers/HomeController.cs
+++ PokeApiWebsite/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PokeApiCore;
 using PokeApiWebsite.Models;
 
 namespace PokeApiWebsite.Controllers
@@ -18,9 +19,36 @@ namespace PokeApiWebsite.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            //Instantiate a PokeApiClient to access the client class
+            PokeApiClient myClient = new PokeApiClient();
+
+            //Get a pokemon by it's id
+            Pokemon result = await myClient.GetPokemonById(1);
+
+            //Grab the moves and assign it to a list so that you can assign it to the MoveList property below
+            List<string> resultMoves = new List<string>();
+            foreach (Move currentMove in result.moves) 
+            {
+                resultMoves.Add(currentMove.move.name);
+            }
+            resultMoves.Sort();
+
+            //TODO: Refactor property names
+
+            //Create an entry object with the data pulled from result variable
+            var entry = new PokedexEntryViewModel()
+            {
+                Id = result.id,
+                Name = result.name,
+                Height = result.height.ToString(),
+                Weight = result.weight.ToString(),
+                PokedexImageUrl = result.sprites.front_default,
+                MoveList = resultMoves
+            };
+
+            return View(entry); //Model binds the entry object to the Index view
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
